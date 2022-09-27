ef5e8625b388661c6c9927f2b9dc0aeb1589dfae
RohanPatel-0 <56854359+RohanPatel-0@users.noreply.github.com>
2022-09-27 12:37:20 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,6 +1,15 @@
 /**
  * this program will take strings in through stdin until stdin is closed.
- * it will then echo back all the string that were entered in a topographically sorted order.
+ * it will then echo back all the strings in a topographically sorted order.
+ * can have a flag -n which indicates numeric sorting, otherwise case-insensitive lexicographic sorting
  */
 int main(int argc, char *argv[]) {
-}
+  if(argc > 1){
+    char* flagChecker;
+    flagChecker = argv[1];
+    if(flagChecker == "-n"){
+      
+    }
+   else{
+   }
+  }
