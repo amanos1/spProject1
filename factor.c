41804d745baadcb350609944fbb17b4184e459cc
RohanPatel-0 <56854359+RohanPatel-0@users.noreply.github.com>
2022-09-27 14:16:51 -0400

    Update factor.c
    Added code for factor.  Check return case, extra space.  STILL NEEDS TESTING.

--- factor.c
+++ factor.c
@@ -1,5 +1,23 @@
 /**
- * This program takes in a number from the command line and returns the prime factor of that number
+ * This program takes in a number from the command line and returns the prime factors of that number
+ * Compile using gcc -g -Wall -Wvla -fsanitize=address factor.c
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
+  //Read in the number to find factors of
+  int toCheck;
+  toCheck = atoi(argv[1]);
+  
+  int i;
+  for(i = 2; toCheck > 1; i++){
+    while(toCheck % i == 0){
+      printf("%d ", i);
+      toCheck = toCheck / i;
+    }
+  }
+  printf("\n")
+  return 0 
 }
