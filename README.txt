4ae6aad6e4d95320e594c19334535de0a65a5802
thing-maker <ajb445@scarletmail.rutgers.edu>
2022-09-27 14:07:41 -0400

    finished monster

---
+++ README.txt
@@ -0,0 +1,2 @@
+Aaron Browne - ajb445
+Rohan Patel - [Rohan, write your nedid here]
--- monster.c
+++ monster.c
@@ -4,6 +4,8 @@
  */
 
 //gcc -g -Wall -Wvla -fsanitize=address monster.c -o monster.exe
+//on windows: gcc -g -Wall -Wvla monster.c -o monster.exe
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,11 +23,11 @@ void printBoard() {
 		for(int j = 0; j < boardX; j++) {
 			if(j > 0)
 				printf(" ");
-			if(i == plrX && j == plrY)
+			if(i == plrY && j == plrX)
 				printf("P");
-			else if(i == goalX && j == goalY)
+			else if(i == goalY && j == goalX)
 				printf("G");
-			else if(i == monX && j == monY)
+			else if(i == monY && j == monX)
 				printf("M");
 			else
 				printf(".");
@@ -33,6 +35,101 @@ void printBoard() {
 				printf(" ");
 		}
 		printf("\n");
+	}
+}
+
+int movePlayer(char dir) {
+	int ugh = 0;
+	switch(dir) {
+		case 'N':
+			if(plrY < boardY-1) {
+				plrY++;
+				ugh = 1;
+			}
+			break;
+		case 'W':
+			if(plrX > 0) {
+				plrX--;
+				ugh = 1;
+			}
+			break;
+		case 'E':
+			if(plrX < boardX-1) {
+				plrX++;
+				ugh = 1;
+			}
+			break;
+		case 'S':
+			if(plrY > 0) {
+				plrY--;
+				ugh = 1;
+			}
+	}
+	if(ugh == 0) {
+		printf("invalid move\n");
+	}
+	return ugh;
+}
+
+int abs(int a) {
+	if(a < 0)
+		return a * -1;
+	return a;
+}
+
+void monsterFollows() {
+	int distX = monX - plrX;
+	int distY = monY - plrY;
+	char dir;
+	if(abs(distX) >= abs(distY)) {
+		if(distX == 0) return;
+		if(distX < 0) {
+			dir = 'E';
+			monX++;
+		} else {
+			dir = 'W';
+			monX--;
+		}
+	} else {
+		if(distY < 0) {
+			dir = 'N';
+			monY++;
+		} else {
+			dir = 'S';
+			monY--;
+		}
+	}
+	printf("monster moves %c\n", dir);
+}
+
+int didWin() {
+	if(plrX == goalX && plrY == goalY) {
+		printf("player wins!\n");
+		return 1;
+	}
+	return 0;
+}
+
+int didLose() {
+	if(plrX == monX && plrY == monY) {
+		printf("monster wins!\n");
+		return 1;
+	}
+	return 0;
+}
+
+void gameLoop() {
+	while(1) {
+		printBoard();
+		int kachiga = 0;
+		do {
+			char command;
+			scanf("%1s", &command);
+			kachiga = movePlayer(command);
+		} while(kachiga == 0);
+		if(didWin()) break;
+		monsterFollows();
+		if(didLose()) break;
 	}
 }
 
@@ -51,7 +148,7 @@ int main(int argc, char *argv[]) {
 	monX = atoi(argv[7]);
 	monY = atoi(argv[8]);
 
-	printBoard();
+	gameLoop();
 
 	return 0;
 }
