79872f873f4afe70e88d2d6126ca41fd596acebb
thing-maker <ajb445@scarletmail.rutgers.edu>
2022-10-03 09:45:21 -0400

    aight grep is almost finished, just one liiiiiiiiiiitle bug.

--- grep.c
+++ grep.c
@@ -4,28 +4,60 @@
  * stdin string if it contains the key in the command line string.
  */
 
+//shit it still prints out statements when letters are separated by spaces, gotta fix that
+
 //gcc -g -Wall -Wvla -fsanitize=address grep.c -o grep
 //on windows: gcc -g -Wall -Wvla grep.c -o grep.exe
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    char *str = malloc(2);
+int contains(char *one, char *two, int oneLen, int caseSens) {
+    int j = 0;
+    for(int i = 0; i <= oneLen; i++) {
+        if(two[j] == '\0') return 0;
+        if(caseSens == 0) {
+            if(one[i] == two[j]) j++;
+        } else {
+            if(one[i] == two[j] || (one[i] - 32) == two[j] || one[i] == (two[j] - 32)) j++;
+        }
+    }
+    return 1;
+}
 
-    if (!str) {
+int checkOption(char *arg) {
+    if(arg[0] == '-' && arg[1] == 'i' && arg[2] == '\0') {
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        printf("No argument provided.\n");
+        return 1;
+    }
+
+    int cap = checkOption(argv[1]);
+
+    int index = 1 + cap;
+
+    char *matchThis = argv[index];
+
+    char *werd = malloc(2);
+
+    if (!werd) {
         printf("Cannot allocate string.\n");
         return 1;
     }
 
     int c;
-    char *reallocStr;
+    char *newWerd;
     int end = 0;
     size_t buf = 2;
 
     while(1) {
         size_t len = 0;
-        printf("Enter some text: ");
 
         while (1) {
             if((c = getchar()) == EOF) {
@@ -35,29 +67,31 @@ int main(int argc, char *argv[]) {
 
             if (len + 1 == buf) {
                 buf *= 2;
-                reallocStr = realloc(str, buf - 1);
+                newWerd = realloc(werd, buf - 1);
 
-                if (!reallocStr) {
+                if (!newWerd) {
                     printf("Cannot reallocate string.\n");
-                    free(str);
+                    free(werd);
                     return 1;
                 }
 
-            str = reallocStr;
+            werd = newWerd;
 
             }
 
-            str[len++] = c;
+            werd[len++] = c;
         }
 
         if(end == 1) break;
 
-        str[len] = '\0';
+        werd[len] = '\0';
 
-        printf("You entered: %s\n", str);
+        if(contains(werd, matchThis, len, cap) == 0) {
+            printf("%s\n", werd);
+        }
     }
 
-    free(str);
+    free(werd);
 
     return 0;
 }
